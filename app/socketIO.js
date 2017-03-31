fd56889e2957880116965b27f528e54692fce2a1
Mague <enmanueldavidmolina@gmail.com>
2017-03-31 17:31:59 -0400

    Cambiado a http

--- app/socketIO.js
+++ app/socketIO.js
@@ -5,13 +5,13 @@ module.exports = function(server){
 	const io_conf = { transports: ['websocket', 'polling'] }
     var sio = require('socket.io')
     // var io = sio.listen(server)
-    var io = sio(server,{ transports: ['websocket', 'polling'] })
+    var io = sio(server,{ transports: ['websocket', 'polling','flashsocket'] })
 
     // io.adapter(sio_redis({ host: conf.redis.host, port: conf.redis.port }))
     io.on('listening',function(socket){
 		console.log("llego una peticion")
     })
-    io.set('origins','*');
+    // io.set('origins','')
     io.on('connection', function (socket) {
     	console.log("Sockets listos")
     	var peer = board.connect();
